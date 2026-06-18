620020af19cd3244045af008c965a8805127514c
shaheerayyaz <shaheerayaz163.com>
2026-06-18 17:07:17 +0500

    Fix image paths for Netlify Deployment

--- components/Header.js
+++ components/Header.js
@@ -27,7 +27,7 @@ export default function Header() {
               width={140}
               height={40}
               priority
-            />
+            /> 
           </Link>
         </div>
 
